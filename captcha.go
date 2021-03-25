694c1b8dafb14580dd409b4274437b8dffa225a5
jake <jdy879526487@aliyun.com>
2021-03-25 11:10:24 +0800

    验证码生成driver一次生成

--- captcha.go
+++ captcha.go
@@ -6,6 +6,7 @@ import (
 	"github.com/mojocn/base64Captcha"
 	"image/color"
 	"io"
+	"sync"
 )
 
 const (
@@ -15,17 +16,25 @@ const (
 	LineOpt       = 3
 )
 
+var (
+	mathCaptcha     *base64Captcha.DriverMath
+	mathCaptchaOnce = &sync.Once{}
+	langCaptcha     *base64Captcha.DriverLanguage
+	langCaptchaOnce = &sync.Once{}
+)
+
 // 生成数学字符串验证码图片
 func MathCaptcha() (id, aws string, img string, err error) {
-	driver := base64Captcha.NewDriverMath(CaptchaHeight, CaptchaWight, NoiseCnt, LineOpt, &color.RGBA{
-		R: uint8(RandIntN(255)),
-		G: uint8(RandIntN(255)),
-		B: uint8(RandIntN(255)),
-		A: uint8(RandIntN(255)),
-	}, []string{"RitaSmith.ttf"})
-
-	id, ques, aws := driver.GenerateIdQuestionAnswer()
-	item, err := driver.DrawCaptcha(ques)
+	mathCaptchaOnce.Do(func() {
+		mathCaptcha = base64Captcha.NewDriverMath(CaptchaHeight, CaptchaWight, NoiseCnt, LineOpt, &color.RGBA{
+			R: uint8(RandIntN(255)),
+			G: uint8(RandIntN(255)),
+			B: uint8(RandIntN(255)),
+			A: uint8(RandIntN(255)),
+		}, []string{"RitaSmith.ttf"})
+	})
+	id, ques, aws := mathCaptcha.GenerateIdQuestionAnswer()
+	item, err := mathCaptcha.DrawCaptcha(ques)
 	if err != nil {
 		return "", "", "", err
 	}
@@ -35,15 +44,16 @@ func MathCaptcha() (id, aws string, img string, err error) {
 
 // 生成中文验证码
 func CharCaptcha() (id, aws string, img io.Reader, err error) {
-	driver := base64Captcha.NewDriverLanguage(CaptchaHeight, CaptchaWight, NoiseCnt, LineOpt, 4, &color.RGBA{
-		R: uint8(RandIntN(255)),
-		G: uint8(RandIntN(255)),
-		B: uint8(RandIntN(255)),
-		A: uint8(RandIntN(255)),
-	}, []*truetype.Font{}, "zh")
-
-	id, ques, aws := driver.GenerateIdQuestionAnswer()
-	item, err := driver.DrawCaptcha(ques)
+	langCaptchaOnce.Do(func() {
+		langCaptcha = base64Captcha.NewDriverLanguage(CaptchaHeight, CaptchaWight, NoiseCnt, LineOpt, 4, &color.RGBA{
+			R: uint8(RandIntN(255)),
+			G: uint8(RandIntN(255)),
+			B: uint8(RandIntN(255)),
+			A: uint8(RandIntN(255)),
+		}, []*truetype.Font{}, "zh")
+	})
+	id, ques, aws := langCaptcha.GenerateIdQuestionAnswer()
+	item, err := langCaptcha.DrawCaptcha(ques)
 	if err != nil {
 		return "", "", nil, err
 	}

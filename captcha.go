78fd6259025c928629cc76e3bbaf0ebd6a1bdb0e
jake <jdy879526487@aliyun.com>
2021-03-25 12:10:45 +0800

    captcha const

--- captcha.go
+++ captcha.go
@@ -6,35 +6,27 @@ import (
 	"github.com/mojocn/base64Captcha"
 	"image/color"
 	"io"
-	"sync"
 )
 
 const (
+	MaxColor      = 2<<7
 	CaptchaHeight = 120
 	CaptchaWight  = 240
 	NoiseCnt      = 2
 	LineOpt       = 3
 )
 
-var (
-	mathCaptcha     *base64Captcha.DriverMath
-	mathCaptchaOnce = &sync.Once{}
-	langCaptcha     *base64Captcha.DriverLanguage
-	langCaptchaOnce = &sync.Once{}
-)
-
 // 生成数学字符串验证码图片
 func MathCaptcha() (id, aws string, img string, err error) {
-	mathCaptchaOnce.Do(func() {
-		mathCaptcha = base64Captcha.NewDriverMath(CaptchaHeight, CaptchaWight, NoiseCnt, LineOpt, &color.RGBA{
-			R: uint8(RandIntN(255)),
-			G: uint8(RandIntN(255)),
-			B: uint8(RandIntN(255)),
-			A: uint8(RandIntN(255)),
-		}, []string{"RitaSmith.ttf"})
-	})
-	id, ques, aws := mathCaptcha.GenerateIdQuestionAnswer()
-	item, err := mathCaptcha.DrawCaptcha(ques)
+	driver := base64Captcha.NewDriverMath(CaptchaHeight, CaptchaWight, NoiseCnt, LineOpt, &color.RGBA{
+		R: uint8(RandIntN(MaxColor)),
+		G: uint8(RandIntN(MaxColor)),
+		B: uint8(RandIntN(MaxColor)),
+		A: uint8(RandIntN(MaxColor)),
+	}, []string{"RitaSmith.ttf"})
+
+	id, ques, aws := driver.GenerateIdQuestionAnswer()
+	item, err := driver.DrawCaptcha(ques)
 	if err != nil {
 		return "", "", "", err
 	}
@@ -44,16 +36,15 @@ func MathCaptcha() (id, aws string, img string, err error) {
 
 // 生成中文验证码
 func CharCaptcha() (id, aws string, img io.Reader, err error) {
-	langCaptchaOnce.Do(func() {
-		langCaptcha = base64Captcha.NewDriverLanguage(CaptchaHeight, CaptchaWight, NoiseCnt, LineOpt, 4, &color.RGBA{
-			R: uint8(RandIntN(255)),
-			G: uint8(RandIntN(255)),
-			B: uint8(RandIntN(255)),
-			A: uint8(RandIntN(255)),
-		}, []*truetype.Font{}, "zh")
-	})
-	id, ques, aws := langCaptcha.GenerateIdQuestionAnswer()
-	item, err := langCaptcha.DrawCaptcha(ques)
+	driver := base64Captcha.NewDriverLanguage(CaptchaHeight, CaptchaWight, NoiseCnt, LineOpt, 4, &color.RGBA{
+		R: uint8(RandIntN(MaxColor)),
+		G: uint8(RandIntN(MaxColor)),
+		B: uint8(RandIntN(MaxColor)),
+		A: uint8(RandIntN(MaxColor)),
+	}, []*truetype.Font{}, "zh")
+
+	id, ques, aws := driver.GenerateIdQuestionAnswer()
+	item, err := driver.DrawCaptcha(ques)
 	if err != nil {
 		return "", "", nil, err
 	}
